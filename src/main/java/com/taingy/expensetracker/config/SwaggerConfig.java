b6ef9019f3a4f6a6835c00d7f35bef196061bb84
taingy.srun <taingysrun.volunteer@rnd4impact.com>
2025-12-24 18:54:23 -0800

    add swagger config

---
+++ src/main/java/com/taingy/expensetracker/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.taingy.expensetracker.config;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("ExpenseTracker API Docs")
+                        .version("1.0.0")
+                        .description("API documentation for ExpenseTracker application.")
+                )
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth",
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)));
+    }
+
+}
+
